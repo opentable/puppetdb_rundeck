00e4273b04b115206d828fe092c46e35f98d4b90
liamjbennett <lbennett@opentable.com>
2014-04-28 12:06:57 +0100

    removing encoding in stub_requests which rails on 1.8 and 1.9

--- spec/helpers/puppetdb_spec.rb
+++ spec/helpers/puppetdb_spec.rb
@@ -6,7 +6,7 @@ describe Helpers::PuppetDB, '#get_nodes' do
   it 'should return json when puppetdb details are valid' do
 
     stub_request(:get, 'http://localhost:8080/v3/nodes').
-        with(:headers => {'Accept'=>['*/*', 'application/json'], 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
+        with(:headers => {'Accept'=>['*/*', 'application/json'], 'User-Agent'=>'Ruby'}).
         to_return(:status => 200, :body => '{ "name" : "test.internal" }', :headers => {})
 
     puppetdb = Helpers::PuppetDB.new('localhost', '8080')
@@ -17,7 +17,7 @@ describe Helpers::PuppetDB, '#get_nodes' do
   it 'should return an empty collection when the details are invalid' do
 
     stub_request(:get, 'http://fubar:8080/v3/nodes').
-        with(:headers => {'Accept'=>['*/*', 'application/json'], 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
+        with(:headers => {'Accept'=>['*/*', 'application/json'], 'User-Agent'=>'Ruby'}).
         to_return(:status => 404, :body => '', :headers => {})
 
     puppetdb = Helpers::PuppetDB.new('fubar', '8080')
@@ -32,7 +32,7 @@ describe Helpers::PuppetDB, '#get_facts' do
   it 'should return json when puppetdb details are valid' do
 
     stub_request(:get, 'http://localhost:8080/v3/facts').
-        with(:headers => {'Accept'=>['*/*', 'application/json'], 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
+        with(:headers => {'Accept'=>['*/*', 'application/json'], 'User-Agent'=>'Ruby'}).
         to_return(:status => 200, :body => '{ "name" : "osfamily" }', :headers => {})
 
     puppetdb = Helpers::PuppetDB.new('localhost', '8080')
@@ -43,7 +43,7 @@ describe Helpers::PuppetDB, '#get_facts' do
   it 'should return an empty collection when the details are invalid' do
 
     stub_request(:get, 'http://fubar:8080/v3/facts').
-        with(:headers => {'Accept'=>['*/*', 'application/json'], 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
+        with(:headers => {'Accept'=>['*/*', 'application/json'], 'User-Agent'=>'Ruby'}).
         to_return(:status => 404, :body => '', :headers => {})
 
     puppetdb = Helpers::PuppetDB.new('fubar', '8080')
